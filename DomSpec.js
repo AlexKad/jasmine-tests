b97994a5c996b99b65ed5a39d0722d2897da48b4
Alex <AlexandraMail@yandex.ru>
2016-07-17 13:56:59 +0300

    Async spec added

--- DomSpec.js
+++ DomSpec.js
@@ -1,13 +1,31 @@
 describe('work with DOM', function(){
-	var renderer;
-	var elementId = "#renderer";
+	var renderer, el;
+
 
 	beforeEach(function(){
-		renderer = new Renderer($(elementId));
+		el = $("<div>sample text</div>");
+		$('body').append(el);
+		renderer = new Renderer(el);
+	});
+
+	afterEach(function(){
+		el.remove();
 	});
 
 	it('should render expected text', function(){
 		renderer.renderName('Alex');
-		expect($(elementId).text()).toBe('Hi, Alex!');
+		expect(el.text()).toBe('Hi, Alex!');
 	})
+
+	describe('async specs', function(){
+		beforeEach(function (done) {
+		    renderer.hideName(done);		   	    	      	    
+		});
+
+		it('should fade out after 1 sec', function(done){
+			expect(el.css('display')).toBe('none');
+			done();
+		});
+
+	});
 });
